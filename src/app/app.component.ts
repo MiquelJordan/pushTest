a9903de2f57b19f484180aeb45f128a9e0a4db5b
MiquelJordan <miquel.jordan.dev@gmail.com>
2018-12-05 15:33:49 +0100

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { LocalNotifications } from "nativescript-local-notifications";
 import { messaging, Message } from "nativescript-plugin-firebase/messaging";
 import { Observable } from "tns-core-modules/data/observable";
-let firebase = require("nativescript-plugin-firebase");
+
 
 @Component({
     moduleId: module.id,
@@ -11,27 +11,27 @@ let firebase = require("nativescript-plugin-firebase");
 })
 export class AppComponent implements OnInit {
     constructor() {
-        // messaging.addOnMessageReceivedCallback(data => {
-        //     console.log("addOnMessageReceivedCallback")
-        //     alert({
-        //         title: "Local Notification received",
-        //         message: `id: '${data}', title: '${data.title}'.`,
-        //         okButtonText: "Roger that"
-        //     });
-        //     LocalNotifications.schedule([{
-        //         id: 1,
-        //         title: 'Sound & Badge',
-        //         body: 'Who needs a push service anyway?',
-        //         badge: 1,
-        //         at: new Date(new Date().getTime() + (5 * 1000)) // 5 seconds from now
-        //     }]);
-        //     console.log(data)
-        // })
+
 
     }
 
     ngOnInit() {
-
+        messaging.addOnMessageReceivedCallback(data => {
+            console.log("addOnMessageReceivedCallback")
+            alert({
+                title: "Local Notification received",
+                message: `id: '${data}', title: '${data.title}'.`,
+                okButtonText: "Roger that"
+            });
+            LocalNotifications.schedule([{
+                id: 1,
+                title: 'Sound & Badge',
+                body: 'Who needs a push service anyway?',
+                badge: 1,
+                at: new Date(new Date().getTime() + (5 * 1000)) // 5 seconds from now
+            }]);
+            console.log(data)
+        })
         console.log("app init")
         // firebase.init({
         //     onMessageReceivedCallback: function (message) {
